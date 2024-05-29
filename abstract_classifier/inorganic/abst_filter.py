ed2f070bb5edc97c59275e870b8f873dec6a0cf2
SungHo_Kim <sungho1067.shk@gmail.com>
2024-05-29 17:24:29 +0900

    Update abst_filter.py

--- abstract_classifier/inorganic/abst_filter.py
+++ abstract_classifier/inorganic/abst_filter.py
@@ -11,10 +11,10 @@ from abstract import BaseClsDataset
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--model", default="./MatBERT_16000", help="The path of the classifier model")
+parser.add_argument("--model", type=str, default="./MatBERT_16000", help="The path of the classifier model")
 parser.add_argument('--input_file_dir', type=str, default="output/")
 parser.add_argument("--postfix", type=str, default="result.txt", help="convert files only if with this postfix")
-parser.add_argument('--device', type=str, default="", type=str, help="Running Device")
+parser.add_argument('--device', type=str, default="", help="Running Device")
 args = parser.parse_args()
 
 
